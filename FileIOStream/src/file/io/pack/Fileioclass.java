77f232c9286e467d9ba98cf13b1f9a74d85bba0d
Prashanth <prashanth0340@gmail.com>
2019-05-17 11:24:24 +0530

    BufferedReader

---
+++ FileIOStream/src/file/io/pack/Fileioclass.java
@@ -0,0 +1,19 @@
+package file.io.pack;
+import java.io.*;
+
+public class Fileioclass {
+	
+	public static void main(String[] args) throws IOException
+	{
+	File f= new File("muni.txt");
+	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("enter your name");
+	while(!br.readLine().contentEquals("stop"))
+	{
+		System.out.println("enter data");
+		System.out.println(br.readLine());
+	}
+	br.close();
+	
+	}
+}
